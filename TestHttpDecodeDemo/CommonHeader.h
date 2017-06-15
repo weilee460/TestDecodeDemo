161048db4a820f628602560345020d4a44c82c2f
lee <wei.lee460@gmail.com>
2017-06-15 18:19:19 +0800

    initial project files.

---
+++ TestHttpDecodeDemo/CommonHeader.h
@@ -0,0 +1,20 @@
+//
+//  CommonHeader.h
+//  TestHttpDecodeDemo
+//
+//  Created by on 2017/6/15.
+//  Copyright © 2017年 lee.ying. All rights reserved.
+//
+
+#ifndef CommonHeader_h
+#define CommonHeader_h
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#endif /* CommonHeader_h */
